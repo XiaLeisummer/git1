15bc95060d6ca53e4933372f98532738c4847f29
xialei <2513202406@qq.com>
2020-09-07 19:07:15 +0800

    抓捕金克斯

--- src/main/java/com/atguigu/test/GitTest3.java
+++ src/main/java/com/atguigu/test/GitTest3.java
@@ -4,5 +4,6 @@ public class GitTest3 {
     public static void main(String[] args) {
         System.out.println("皮尔特沃夫的地下电梯");
         System.out.println("金克斯和蔚的恩怨");
+        System.out.println("城市仓库的追逐战");
     }
 }
